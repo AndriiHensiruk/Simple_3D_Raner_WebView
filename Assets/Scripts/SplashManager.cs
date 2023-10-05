f26698aa39fe40a62c975e7956cc38f91486da3d
devHensiruk <Hensiruk@gmail.com>
2023-10-05 12:34:05 +0300

    added system of loading levels with the help of playerpref

---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour
+{
+    public Button playGame;
+    public Button webView;
+    bool gameComplete;
+
+    void Start()
+    {
+        gameComplete = PlayerPrefs.GetInt("GameComplete") == 1 ? false : true;
+        if (gameComplete)
+        {
+            playGame.gameObject.SetActive(true);
+            webView.gameObject.SetActive(false);
+        }
+        else
+        {
+            playGame.gameObject.SetActive(false);
+            webView.gameObject.SetActive(true);
+
+        }
+
+    }
+    public void LoadTo(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
